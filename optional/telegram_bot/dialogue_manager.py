6261dbdda71d55cdd7c337dafe671e93f7774edc
Komissarov Semyon <s.komissarov@corp.mail.ru>
2020-10-30 13:08:41 +0300

    update function from main branch

--- optional/telegram_bot/dialogue_manager.py
+++ optional/telegram_bot/dialogue_manager.py
@@ -1,14 +1,18 @@
 import os
+
 from sklearn.metrics.pairwise import pairwise_distances_argmin
 
 from chatterbot import ChatBot
-from utils import *
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from utils import load_embeddings, unpickle_file, question_to_vec, text_prepare
 
 
 class ThreadRanker(object):
     def __init__(self, paths):
-        self.word_embeddings, self.embeddings_dim = load_embeddings(paths['WORD_EMBEDDINGS'])
-        self.thread_embeddings_folder = paths['THREAD_EMBEDDINGS_FOLDER']
+        self.word_embeddings, self.embeddings_dim = load_embeddings(
+            paths["WORD_EMBEDDINGS"]
+        )
+        self.thread_embeddings_folder = paths["THREAD_EMBEDDINGS_FOLDER"]
 
     def __load_embeddings_by_tag(self, tag_name):
         embeddings_path = os.path.join(self.thread_embeddings_folder, tag_name + ".pkl")
@@ -16,17 +20,21 @@ class ThreadRanker(object):
         return thread_ids, thread_embeddings
 
     def get_best_thread(self, question, tag_name):
-        """ Returns id of the most similar thread for the question.
-            The search is performed across the threads with a given tag.
+        """Returns id of the most similar thread for the question.
+        The search is performed across the threads with a given tag.
         """
         thread_ids, thread_embeddings = self.__load_embeddings_by_tag(tag_name)
 
         # HINT: you have already implemented a similar routine in the 3rd assignment.
-        
-        question_vec = #### YOUR CODE HERE ####
-        best_thread = #### YOUR CODE HERE ####
-        
-        return thread_ids[best_thread]
+
+        question_vec = question_to_vec(
+            question, self.word_embeddings, self.embeddings_dim
+        )
+        best_thread = pairwise_distances_argmin(
+            thread_embeddings, question_vec.reshape(1, -1)
+        )
+
+        return thread_ids[best_thread[0]]
 
 
 class DialogueManager(object):
@@ -34,50 +42,53 @@ class DialogueManager(object):
         print("Loading resources...")
 
         # Intent recognition:
-        self.intent_recognizer = unpickle_file(paths['INTENT_RECOGNIZER'])
-        self.tfidf_vectorizer = unpickle_file(paths['TFIDF_VECTORIZER'])
+        self.intent_recognizer = unpickle_file(paths["INTENT_RECOGNIZER"])
+        self.tfidf_vectorizer = unpickle_file(paths["TFIDF_VECTORIZER"])
 
-        self.ANSWER_TEMPLATE = 'I think its about %s\nThis thread might help you: https://stackoverflow.com/questions/%s'
+        self.ANSWER_TEMPLATE = "I think its about %s\nThis thread might help you: " \
+                               "https://stackoverflow.com/questions/%s"
 
         # Goal-oriented part:
-        self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
+        self.tag_classifier = unpickle_file(paths["TAG_CLASSIFIER"])
         self.thread_ranker = ThreadRanker(paths)
+        self.__init_chitchat_bot()
 
-    def create_chitchat_bot(self):
+    def __init_chitchat_bot(self):
         """Initializes self.chitchat_bot with some conversational model."""
 
         # Hint: you might want to create and train chatterbot.ChatBot here.
-        # It could be done by creating ChatBot with the *trainer* parameter equals 
-        # "chatterbot.trainers.ChatterBotCorpusTrainer"
-        # and then calling *train* function with "chatterbot.corpus.english" param
-        
-        ########################
-        #### YOUR CODE HERE ####
-        ########################
-       
+        # Create an instance of the ChatBot class.
+        # Create a trainer (chatterbot.trainers.ChatterBotCorpusTrainer) for the ChatBot
+        # Train the ChatBot with "chatterbot.corpus.english" param.
+        self.chitchat_bot = ChatBot("Ron Obvious")
+        self.chitchat_bot.set_trainer(ChatterBotCorpusTrainer)
+        self.chitchat_bot.train("chatterbot.corpus.english")
+
     def generate_answer(self, question):
         """Combines stackoverflow and chitchat parts using intent recognition."""
 
-        # Recognize intent of the question using `intent_recognizer`.
-        # Don't forget to prepare question and calculate features for the question.
-        
-        prepared_question = #### YOUR CODE HERE ####
-        features = #### YOUR CODE HERE ####
-        intent = #### YOUR CODE HERE ####
+        # Recognize intent of the question using `intent_recognizer`
+        # Don't forget to prepare question and calculate features for the question
 
-        # Chit-chat part:   
-        if intent == 'dialogue':
-            # Pass question to chitchat_bot to generate a response.       
-            response = #### YOUR CODE HERE ####
+        prepared_question = text_prepare(question)
+        features = self.tfidf_vectorizer.transform(
+            [prepared_question]
+        )
+        intent = self.intent_recognizer.predict(features)[0]
+        # Chit-chat part:
+        if intent == "dialogue":
+            # Pass question to chitchat_bot to generate a response.
+            response = self.chitchat_bot.get_response(question)
             return response
-        
-        # Goal-oriented part:
-        else:        
-            # Pass features to tag_classifier to get predictions.
-            tag = #### YOUR CODE HERE ####
-            
-            # Pass prepared_question to thread_ranker to get predictions.
-            thread_id = #### YOUR CODE HERE ####
-           
-            return self.ANSWER_TEMPLATE % (tag, thread_id)
 
+        # Goal-oriented part:
+        else:
+            # Pass features to tag_classifier to get predictions.
+            tag = self.tag_classifier.predict(features)[0]
+
+            # Pass prepared_question to thread_ranker to get predictions.
+            thread_id = self.thread_ranker.get_best_thread(
+                prepared_question, tag
+            )
+
+            return self.ANSWER_TEMPLATE % (tag, thread_id)
--- optional/telegram_bot/utils.py
+++ optional/telegram_bot/utils.py
@@ -2,6 +2,7 @@ import nltk
 import pickle
 import re
 import numpy as np
+import pandas as pd
 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -12,20 +13,19 @@ RESOURCE_PATH = {
     'TAG_CLASSIFIER': 'tag_classifier.pkl',
     'TFIDF_VECTORIZER': 'tfidf_vectorizer.pkl',
     'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags',
-    'WORD_EMBEDDINGS': 'word_embeddings.tsv',
+    'WORD_EMBEDDINGS': '/content/drive/My Drive/Colab Notebooks/sodd.tsv',
 }
 
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
-
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
-    bad_symbols_re = re.compile('[^0-9a-z #+_]')
+    good_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
 
     text = text.lower()
     text = replace_by_space_re.sub(' ', text)
-    text = bad_symbols_re.sub('', text)
+    text = good_symbols_re.sub('', text)
     text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
 
     return text.strip()
@@ -33,10 +33,8 @@ def text_prepare(text):
 
 def load_embeddings(embeddings_path):
     """Loads pre-trained word embeddings from tsv file.
-
     Args:
       embeddings_path - path to the embeddings file.
-
     Returns:
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
@@ -45,32 +43,25 @@ def load_embeddings(embeddings_path):
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
-
-    ########################
-    #### YOUR CODE HERE ####
-    ########################
-
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    model = pd.read_csv(embeddings_path, sep='\t', header=None, index_col=0)
+    size = model.shape[1]
+    model = model.T.to_dict(orient='list')
+    starspace_embeddings = {k: np.array(model[k], dtype=np.float32) for k in model}
+    return starspace_embeddings, size
 
 
 def question_to_vec(question, embeddings, dim):
     """Transforms a string to an embedding by averaging word embeddings."""
 
     # Hint: you have already implemented exactly this function in the 3rd assignment.
-
-    ########################
-    #### YOUR CODE HERE ####
-    ########################
-
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    words_embeddings = []
+    for w in question.split():
+        if w in embeddings:
+            words_embeddings.append(embeddings[w])
+    if words_embeddings:
+        return np.mean(words_embeddings, axis=0)
+    else:
+        return np.zeros(dim)
 
 
 def unpickle_file(filename):
